184fb149ff1b8cff5ba7db4cbcecdba6b85d2fbe
Fikri Rasyid <fikrirasyid@gmail.com>
2015-01-05 00:44:23 +0700

    More intuitive Jetpack site-logo implementation
    Site title will be automatically hidden if site logo is added

--- functions.php
+++ functions.php
@@ -18,3 +18,16 @@ function twentyfifteen_with_jetpack(){
 
 }
 add_action( 'after_setup_theme', 'twentyfifteen_with_jetpack' );
+
+/**
+ * Adding custom field for theme's customizer
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function twentyfifteen_with_jetpack_customize_register( $wp_customize ) {
+
+	// Remove  header textcolor control
+	$wp_customize->remove_control( 'display_header_text' );
+
+}
+add_action( 'customize_register', 'twentyfifteen_with_jetpack_customize_register' );
