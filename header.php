57aad424e0fd0b67c94d09459538831313a13734
Fikri Rasyid <fikrirasyid@gmail.com>
2015-01-04 18:28:07 +0700

    Correct way to implement jetpack_the_site_logo()
    Apparently, jetpack_the_site_logo() is meant to be added without
    replacing the site-logo. site-logo can be hidden from customizer

--- header.php
+++ header.php
@@ -30,19 +30,15 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<?php
-					if ( function_exists( 'jetpack_the_site_logo' ) && function_exists( 'jetpack_has_site_logo' ) && jetpack_has_site_logo() ) {
-
+					if ( function_exists( 'jetpack_the_site_logo' ) ) {
 						jetpack_the_site_logo();
-
-					} else {
-
-						if ( is_front_page() && is_home() ) : ?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php else : ?>
-							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php endif;
-
 					}
+
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif;					
 
 					$description = get_bloginfo( 'description', 'display' );
 					if ( $description || is_customize_preview() ) : ?>
